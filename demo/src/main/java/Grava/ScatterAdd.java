491533629185a8ac65eeb47fd2a6503ebef0b07a
bin ladin <samraj19990684@gmail.com>
2022-12-17 12:46:04 +0000

    allah akbara

--- demo/src/main/java/Grava/ScatterAdd.java
+++ demo/src/main/java/Grava/ScatterAdd.java
@@ -5,6 +5,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import org.jfree.chart.axis.NumberAxis;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
@@ -13,16 +17,25 @@ import javafx.stage.Stage;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.fx.ChartViewer;
+import org.jfree.chart.panel.CrosshairOverlay;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.Crosshair;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.statistics.Regression;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
+import java.awt.*;
+
 public class ScatterAdd extends Application {
 
 
     private final XYSeries series = new XYSeries("Voltage");
+    private final XYSeries trend = new XYSeries("Trend");
     private final XYSeriesCollection dataset = new XYSeriesCollection(series);
 
     ChoiceBox<String> domainLabels = new ChoiceBox<>();
@@ -31,7 +44,6 @@ public class ScatterAdd extends Application {
     private JFreeChart createChart() {
         XYSeriesCollection dataset = new XYSeriesCollection();
         dataset.addSeries(series);
-
         return ChartFactory.createScatterPlot("VI Characteristics", "Current", "Voltage", dataset);
     }
 
@@ -42,21 +54,29 @@ public class ScatterAdd extends Application {
         stage.getIcons().add(image);
 
 
-        ChoiceBox<String> domainLabels = new ChoiceBox<>();
-        ChoiceBox<String> rangeLabels = new ChoiceBox<>();
-
         XYPlot plot = createChart().getXYPlot();
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+        /*
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+        adjustAxis((NumberAxis) plot.getDomainAxis(), true);
+        adjustAxis((NumberAxis) plot.getRangeAxis(), false);
+        */
         XYLineAndShapeRenderer r = (XYLineAndShapeRenderer) plot.getRenderer();
         r.setSeriesLinesVisible(1, Boolean.TRUE);
         r.setSeriesShapesVisible(1, Boolean.FALSE);
 
-        series.addChangeListener((event) -> {
-                    double[] coefficients = Regression.getOLSRegression(dataset, 0);
-                    double b = coefficients[0]; // intercept
-                    double m = coefficients[1]; // slope
-                    System.out.println("y = " + m + " x + " + b);
+        var equation = new TextField();
 
-                });
+        series.addChangeListener((event) -> {
+            double[] coefficients = Regression.getOLSRegression(dataset, 0);
+            double b = coefficients[0]; // intercept
+            double m = coefficients[1]; // slope
+            equation.setText("y = " + m + " x + " + b);
+
+        });
+
         JFreeChart chart = createChart();
         domainLabels.getSelectionModel().selectedItemProperty().addListener((ov, s0, s1) -> {
             chart.getXYPlot().getDomainAxis().setLabel(s1);
@@ -71,12 +91,11 @@ public class ScatterAdd extends Application {
         rangeLabels.getItems().addAll("Voltage", "Metres");
         rangeLabels.setValue("Voltage");
 
-
-        var xSpin = new Spinner<Double>(-10000000.000, 10000000.000, 0,0.1);
+        var xSpin = new Spinner<Double>(-10000000.000, 10000000.000, 0, 0.1);
         xSpin.setEditable(true);
         xSpin.setPromptText("Xvalue");
 
-        var ySpin = new Spinner<Double>(-10000000.000, 10000000.000, 0,0.1);
+        var ySpin = new Spinner<Double>(-10000000.000, 10000000.000, 0, 0.1);
         ySpin.setEditable(true);
         ySpin.setPromptText("Yvalue");
 
@@ -90,7 +109,7 @@ public class ScatterAdd extends Application {
         HBox yBox = new HBox();
         yBox.getChildren().addAll(rangeLabels);
 
-        var enter = new ToolBar(xBox, xSpin, yBox, ySpin, button);
+        var enter = new ToolBar(xBox, xSpin, yBox, ySpin, button, equation);
         BorderPane.setAlignment(enter, Pos.CENTER);
 
         BorderPane root = new BorderPane();
@@ -98,10 +117,17 @@ public class ScatterAdd extends Application {
         root.setBottom(enter);
 
         stage.setTitle("ScatterAdd");
-        stage.setScene(new Scene(root, 640, 480));
+        stage.setScene(new Scene(root, 720, 480));
         stage.show();
 
     }
+    /*
+    private void adjustAxis(NumberAxis axis, boolean vertical) {
+        axis.setRange(-3.0, 3.0);
+        axis.setTickUnit(new NumberTickUnit(0.5));
+        axis.setVerticalTickLabels(vertical);
+    }
+*/
 
     public static void main(String[] args) {
         launch(args);
