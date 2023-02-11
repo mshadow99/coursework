534f271f5a9248021e17530a448a4bfb3f5c5394
mshadow99 <95759984+mshadow99@users.noreply.github.com>
2023-02-11 15:09:21 +0000

    Update Calc.java

--- demo/src/main/java/Calc.java
+++ demo/src/main/java/Calc.java
@@ -10,31 +10,35 @@ public class Calc extends Grava {
         double sigmaXX = 0.0;
         double sigmaYY = 0.0;
         double sigmaXY = 0.0;
-
+        //sigma values instantiated
         int n = xList.size();
-
+        //variable n created(length of list)
         for (int i = 0; i < n; ++i) {
             double x = (double) xList.get(i);
             double y = (double) yList.get(i);
-
-            sigmaX = sigmaX + x;
-            sigmaY = sigmaY + y;
-            sigmaXX = sigmaXX + (x * x);
-            sigmaYY = sigmaYY + (y * y);
-            sigmaXY = sigmaXY + (x * y);
-
+            //both the value at x and y are created x,y
+            sigmaX += x;
+            sigmaY += y;
+            //the x and y values are added through the length of the list to calculate the sum
+            sigmaXX += (x * x);
+            sigmaYY += + (y * y);
+            //the x and y values are squared then added through the length of the list
+            sigmaXY += + (x * y);
+            //the x and y values are multiplied in order to find the sum of the x*y values.
 
         }
 
         double sigmaX2 = sigmaX * sigmaX;
         double sigmaY2 = sigmaY * sigmaY;
-        //double sigmaX2 = sigmaX * sigmaX;
-        //double sigmaY2 = sigmaY * sigmaY;
         double sigmaXY2 = sigmaX * sigmaY;
+
         double numerator = (n * sigmaXY) - (sigmaXY2);
         double denominator = Math.sqrt(((n * (sigmaXX)) - (sigmaX2)) * (n * (sigmaYY) - (sigmaY2)));
         double rVal = (numerator / denominator);
-
+        //the correlation coefficient is calculated here with the pearson correlation coefficient formula
+        //  covariance = (n* (sum of x*y values))-(sum of x values * sum of y values)
+        // Standard deviation of x * standard deviation of y = ( n * (sum of x*x values) - (sum of x values)*(sum of x values) ) * ((n * (sum of y*y values)) - ((sum of y values)*(sum of y values)))
+        // rVal(correlation coefficient) = covariance / (Standard deviation of x * standard deviation of y)
         return rVal;
 
     }
@@ -43,33 +47,33 @@ public class Calc extends Grava {
         double sigmaX = 0.00;
         double sigmaY = 0.00;
         double sigmaXX = 0.00;
-        double sigmaYY = 0.00;
         double sigmaXY = 0.00;
-
+        //sigma values instantiated
         int n = xList.size();
-
+        //variable n created(length of list)
         for (int i = 0; i < n; i++) {
             double x = (double) xList.get(i);
             double y = (double) yList.get(i);
-
-            sigmaX = sigmaX + x;
-            sigmaY = sigmaY + y;
-            sigmaXX = sigmaXX + (x * x);
-            sigmaYY = sigmaYY + (y * y);
-            sigmaXY = sigmaXY + (x * y);
+            //both the value at x and y are created x,y
+            sigmaX += x;
+            sigmaY += y;
+            //the x and y values are added through the length of the list to calculate the sum
+            sigmaXX += (x * x);
+            //the x values are squared then added through the length of the list
+            sigmaXY += + (x * y);
+            //the x and y values are multiplied in order to find the sum of the x*y values.
 
         }
         double sigmaX2 = sigmaX * sigmaX;
         double sigmaXY2 = sigmaX * sigmaY;
 
 
-
-
         double numerator = ((n * sigmaXY) - (sigmaXY2));
         double denominator = ((n * sigmaXX) - (sigmaX2));
         double b = (numerator / denominator);
-
-
+        // numerator = (n * (sum of x*y values)) - (sum of x values * sum of y values)
+        // denominator = (n * sum of x*x values) - (sum of x values * sum of x values)
+        //least squares regression(b) = numerator/ denominator
 
         return b;
 
@@ -81,26 +85,35 @@ public class Calc extends Grava {
         double sigmaXX = 0.00;
         double sigmaYY = 0.00;
         double sigmaXY = 0.00;
-
+        //sigma values instantiated
         int n = xList.size();
-
+        //variable n created(length of list)
         for (int i = 0; i < n; i++) {
             double x = (double) xList.get(i);
             double y = (double) yList.get(i);
+            //both the value at x and y are created x,y
             sigmaX = sigmaX + x;
             sigmaY = sigmaY + y;
+            //the x and y values are added through the length of the list to calculate the sum
             sigmaXX = sigmaXX + (x * x);
             sigmaYY = sigmaYY + (y * y);
+            //the x and y values are squared then added through the length of the list
             sigmaXY = sigmaXY + (x * y);
+            //the x and y values are multiplied in order to find the sum of the x*y values.
         }
         double sigmaX2 = sigmaX * sigmaX;
         double sigmaXY2 = sigmaX * sigmaY;
         double meanX = sigmaX/n;
         double meanY = sigmaY/n;
+        // mean = sum of list/ total number of items in list
         double numerator = ((n * sigmaXY) - (sigmaXY2));
         double denominator = ((n * sigmaXX) - (sigmaX2));
         double b = (numerator / denominator);
+        // numerator = (n * (sum of x*y values)) - (sum of x values * sum of y values)
+        // denominator = (n * sum of x*x values) - (sum of x values * sum of x values)
+        //least squares regression(b) = numerator/ denominator
         double a = meanY - (b*meanX);
+        // c(intercept) = y - m*x
         return a;
     }
 }
