8b5f1e25ba58d99346d883b8a3e532a0850814f3
mshadow99 <95759984+mshadow99@users.noreply.github.com>
2023-02-12 23:47:33 +0000

    Update test.java

--- demo/src/main/java/test.java
+++ demo/src/main/java/test.java
@@ -2,28 +2,31 @@ package Grava;
 
 import javafx.beans.property.SimpleDoubleProperty;
 
-public class test {
+public class tableClass {
+
     private SimpleDoubleProperty xCol;
     private SimpleDoubleProperty yCol;
-
-    public test(double xCol, double yCol) {
+    // private property objects yCol and xCol created
+    public tableClass(double xCol, double yCol) {
         this.xCol = new SimpleDoubleProperty(xCol);
         this.yCol = new SimpleDoubleProperty(yCol);
+        //returns value of xCol and yCol
     }
 
-    public double getX() {
-        return this.xCol.get();
-    }
+    public double getX(){
+        return xCol.get();
+}
 
-    public void setX(int x) {
-        this.xCol = new SimpleDoubleProperty((double)x);
-    }
+public void setX(int x){
+        this.xCol = new SimpleDoubleProperty (x);
+}
 
-    public double getY() {
-        return this.yCol.get();
-    }
+public double getY(){
+        return yCol.get();
+}
 
-    public void setY(int y) {
-        this.yCol = new SimpleDoubleProperty((double)y);
-    }
+public void setY(int y){
+        this.yCol = new SimpleDoubleProperty(y);
+}
+//getters and setters used to obtain value in the Grava class
 }
