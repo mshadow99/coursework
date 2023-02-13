ba9c5abc253204904f283ef8a1c435a785c943f6
mshadow99 <95759984+mshadow99@users.noreply.github.com>
2023-02-13 00:06:33 +0000

    Update Grava.java

--- demo/src/main/java/Grava.java
+++ demo/src/main/java/Grava.java
@@ -54,40 +54,43 @@ public class Grava extends Application {
     //obtains the value of the rangeLabels
     String xLabel = domainLabels.getValue();
     //obtains the value of the domainLabels
-    private ObservableList<test> testModel = FXCollections.observableArrayList(
+    private ObservableList<tableClass> testModel = FXCollections.observableArrayList(
     );
     //a list is created to hold the values inputted to be displayed to the table
     private JFreeChart createChart() {
         return ChartFactory.createScatterPlot("","Amps", "Volts", dataset);
     }
-
+    //a scatterplot(createChart) is created and is a method object from the chartfactory class with name createChart
     @Override
     public void start(Stage stage) {
-
+        //the start method is created from the application class with parameters stage
         Image image = new Image("Grava.logo.png");
         stage.getIcons().add(image);
-        var chart = createChart();
+        // the applications image icon is set
+        HBox xBox = new HBox();
+        HBox yBox = new HBox();
+        //creates x and yBox application layouts
 
+        var chart = createChart();
+        //creates object of create chart method
 
         XYPlot plot = chart.getXYPlot();
+        //creates object plot of getXYPlot() method
         dataset.addSeries(trend);
-
-        ValueMarker marker = new ValueMarker(0);  // position is the value on the axis
+        //add the series trend to the dataset
+        ValueMarker marker = new ValueMarker(0);
+        // creates an object(marker of the ValueMarker() class with position 0,0
         marker.setPaint(Color.black);
-
+        // sets color of the axis as black
         plot.addDomainMarker(marker);
         plot.addRangeMarker(marker);
+        //adds the domain line and the range line to the plot
 
-
-
-        plot.setRangeCrosshairValue(0.0);
-        plot.setDomainCrosshairValue(0.0);
-        plot.setDomainCrosshairLockedOnData(true);
         XYLineAndShapeRenderer r = (XYLineAndShapeRenderer) plot.getRenderer();
-
+        // creates an object of the .getRenderer method which is a straight line
         r.setSeriesLinesVisible(1, Boolean.TRUE);
         r.setSeriesShapesVisible(1, Boolean.FALSE);
-
+        //sets the lines visibility as true and the lines plot shapes as false
 
 
         var equationField = new TextField();
@@ -99,8 +102,17 @@ public class Grava extends Application {
         equationField.setEditable(false);
         gradientField.setEditable(false);
         corField.setEditable(false);
-        table = new TableView();
 
+        var deleteButton = new Button("clear last value");
+        var addButton = new Button("Add");
+        var clearButton = new Button("clear List");
+        //button objects created
+
+        var cb = new CheckBox("Trend");
+        //creates a checkbox with label Trend
+        //text-fields are created to view outputted data
+        table = new TableView();
+        // creates a table object of the TableView() class.
         var xSpin = new Spinner<Double>(-10000000.000, 10000000.000, 0, 0.1);
         xSpin.setEditable(true);
         xSpin.setPromptText("Xvalue");
@@ -108,43 +120,46 @@ public class Grava extends Application {
         var ySpin = new Spinner<Double>(-10000000.000, 10000000.000, 0, 0.1);
         ySpin.setEditable(true);
         ySpin.setPromptText("Yvalue");
-
-
-        //System.out.println(xList.size());
+        // here both x and ySpin objects of spinner are created,where x and y values can be inputted
+        // parameters of the spinners being (min value, max value, initial value, step count)
 
         TableColumn xCol = new TableColumn("X:");
         TableColumn yCol = new TableColumn("Y:");
         xCol.setMinWidth(150);
         yCol.setMinWidth(150);
+        // here xCol and yCol objects with minimum width 150p  are created which hold the x and y values inputted
         xCol.setCellValueFactory(new PropertyValueFactory<>("X"));
         yCol.setCellValueFactory(new PropertyValueFactory<>("Y"));
-
+        // here the xCol and yCol properties are set as X and Y
         DecimalFormat df = new DecimalFormat("0.000");
-
+        // here a decimalFormat object is created to 3 decimal places
 
         series.addChangeListener((event) -> {
             double spinX = xSpin.getValue();
             double spinY = ySpin.getValue();
-
+            // the double value of the spinner is set as spinX and spinY
 
             if (series.getItemCount() > 1) {
-                double[] coefficients = Regression.getOLSRegression(dataset, 0);
-                double c = coefficients[0]; // intercept
-                double m = coefficients[1]; // slope
+                // if statement to ensure there is a minimum of 2 values in the series
                 double b = Calc.leastSquare();
+                // gradient from the least square regression formula
                 double a = Calc.intercept();
-                float formatM = Float.parseFloat(df.format(m));
-                float formatC = Float.parseFloat(df.format(c));
+                // y intercept calculated with rearranging the y = mx + c formula
                 float formatB = Float.parseFloat(df.format(b));
                 float formatA = Float.parseFloat(df.format(a));
-
+                // the values of a and b are formatted to 3 decimal places
                 equationField.setText("y = " +  formatB + " + " + formatA);
-
+                // the equation of the line is displayed to the text-field
                 double x = series.getDataItem(0).getXValue();
+                //the x value at index 0 is set to x
                 trend.clear();
-                trend.add(x, m * x + c);
+                // the trend series is cleared
+                trend.add(x, b * x + a);
+                //points x and y are added to the trend series
                 x = series.getDataItem(series.getItemCount() - 1).getXValue();
-                trend.add(x, m * x + c);
+                //x is set to the most recent item in the trend series
+                trend.add(x, b * x + a);
+                // the most recent value in the series is added to the trend
             }
         });
 
@@ -158,33 +173,38 @@ public class Grava extends Application {
             chart.getXYPlot().getRangeAxis().setLabel(s1);
             yCol.setText(s1);
         });
-
+        // listeners created to change the charts axis labels and the table column titles in accordance with the choicebox labels
 
         domainLabels.getItems().addAll("Amps", "Seconds","Second","Metres","Extension","Strain","Kilograms","Metres³","Volts","Metres²");
         domainLabels.setValue("xLabel");
-
+        //
         rangeLabels.getItems().addAll("Volts", "Metres","Metres/Second","Joules","Force","Stress","Momentum","Coulombs","Ohms","Pascals");
         rangeLabels.setValue("yLabel");
+        //the choiceboxes are populated with items
 
-
-
-        var addButton = new Button("Add");
         addButton.setOnAction(ae -> {
             double spinX = xSpin.getValue();
             double spinY = ySpin.getValue();
             xList.add(spinX);
             yList.add(spinY);
-            testModel.add(new test(spinX, spinY));
+            testModel.add(new tableClass(spinX, spinY));
             series.add(spinX, spinY);
+            // the double value of the spinner is added to the x and yLists, the testModel observableArrayList and the series
 
 
             if (series.getItemCount() > 1){
-                double m = Calc.leastSquare(); // slope
-                float roundedCore = Float.parseFloat(df.format(Calc.correlation()));
+                double m = Calc.leastSquare();
+                double cor = Calc.correlation();
+                // objects created of methods of the calc class
+                float roundedCor = Float.parseFloat(df.format(cor));
                 float roundedM = Float.parseFloat(df.format(m));
+                // float values to 3 decimal places created
                 yLabel = rangeLabels.getValue();
                 xLabel = domainLabels.getValue();
-                corField.setText(String.valueOf(roundedCore));
+                // the value of the choiceboxes set as x and yLabel
+                corField.setText(String.valueOf(roundedCor));
+                // correlation coefficient outputted to textfield
+
                 if ((xLabel == ("Amps")) && (yLabel == ("Volts"))) {
 
                     gradientField.setText(roundedM +"Ω");
@@ -227,35 +247,34 @@ public class Grava extends Application {
                 } else {
                     gradientField.setText(String.valueOf(roundedM));
                 }
-
+                // multiple conditions created if that the x and y label match then the gradient gives a meaningful value(unit) if not then gradient displayed
 
             }
         });
 
-        var deleteButton = new Button("clear last value");
+
         deleteButton.setOnAction(ae -> {
-            double i = testModel.size() - 1;
-            double z = series.getItemCount();
-            ObservableList<test> allSelected, itemSelected;
+            ObservableList<tableClass> allSelected, itemSelected;
             allSelected = table.getItems();
             itemSelected = table.getSelectionModel().getSelectedItems();
             Object itemSelected1 = table.getSelectionModel().getSelectedItem();
-            if (itemSelected1 != null){
+            //2 list objects are created of the table
+            if (itemSelected1 != null){// condition to check if an item is selected
 
                 int n = (table.getSelectionModel().getSelectedIndex());
+                //returns the index of the currently selected item in the table
 
-
-                if (series.getItemCount() == 1) {
+                if (series.getItemCount() == 1) {// boolean expression to check if length of series is = to 1
                     series.clear();
                     trend.clear();
                     testModel.clear();
+                    // clears series,trend and testModel list
                 }else {
                     series.delete(n, n);
                     itemSelected.forEach(allSelected::remove);
-                    //table.refresh();
-
                     xList.remove(n);
                     yList.remove(n);
+                    //removes data item from the series with index n, removes item selected from the table and removes data items with index n from x and yList
 
                 }
 
@@ -263,53 +282,54 @@ public class Grava extends Application {
 
         } );
 
-        var clearButton = new Button("clear List");
         clearButton.setOnAction(e -> {
             e.consume();
             clearList();
+            //runs the clearList() method if the clearButton is selected
         });
 
         var g = chart.getXYPlot().getRenderer();
-        var cb = new CheckBox("Trend");
-        cb.setSelected(true);
-        cb.selectedProperty().addListener((o) -> {
-            //if (series.getItemCount() > 0) {
-            g.setSeriesVisible(1, cb.isSelected());
+        // creates g object pf XYItemRenderer
 
-            //}
+        //creates
+        cb.setSelected(true);//checkbox ticked
+        cb.selectedProperty().addListener((o) -> {
+            // listener lambda expression used to check if the checkbox is ticked
+            g.setSeriesVisible(1, cb.isSelected());
+            // changes the visibility of the trend series when the checkbox is selected
         });
 
         table.setItems(testModel);
         table.getColumns().addAll(yCol,xCol);
+        //the list tesModel is added to the table and the columns are added to the table
 
-        HBox xBox = new HBox();
         xBox.getChildren().addAll(domainLabels);
-
-        HBox yBox = new HBox();
         yBox.getChildren().addAll(rangeLabels);
-
+        //adds the choiceboxes to the application
         var enter = new ToolBar(cb,xBox, xSpin, yBox, ySpin, addButton, equationField,gradientField ,corField,deleteButton,clearButton);
+        //toolbar layout created and populated with items
         BorderPane.setAlignment(enter, Pos.CENTER);
-
         BorderPane root = new BorderPane();
         root.setCenter(new ChartViewer(chart));
         root.setRight(table);
         root.setBottom(enter);
-
+        //root object created of BorderPane() class and the 3 items table,chart and enter are added
         stage.setTitle("Grava");
         stage.setScene(new Scene(root, 1280, 700));
         stage.setMinHeight(500);
         stage.setMinWidth(900);
         stage.show();
-
+        //the applications title and size is set
     }
 
     private void clearList(){
         Boolean answer = ConfirmBox.display("Clear","Are you sure you want to clear all values?");
+        // answer created to confirm button press
         if(answer) {
             series.clear();
             trend.clear();
             testModel.clear();
+            // if the answer = true then the series,trend and testmodel list is cleared
         }
     }
 
